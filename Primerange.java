925df701fd7872000ab813b88829b8151100f976
vsaipavan6 <84652385+vsaipavan6@users.noreply.github.com>
2021-07-15 11:49:12 +0530

    Create Primerange.java

---
+++ Primerange.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class twin_Prime
+
+ {
+
+   static boolean prime(int n)
+
+   {
+
+      int count=0;
+
+      for(int i=1;i<=n;i++)
+
+      {
+
+        if(n%i==0)
+
+        count++;
+
+      }
+
+      if(count==2)
+
+      return true;
+
+      else
+
+      return false;
+
+   }
+
+ public static void main (String args[])
+
+ {  
+
+     int count1=0;
+
+    Scanner sc=new Scanner(System.in);
+
+    System.out.print("Enter n1:");
+
+    int n1=sc.nextInt();
+
+    System.out.print("Enter n2:");
+
+    int n2=sc.nextInt();
+
+    for(int i=n1;i<=n2;i++)
+
+    {
+
+     if(prime(i)&&prime(i+2)==true)
+
+       count1++;
+
+    }
+
+    System.out.print(count1);
+
+ }
+
+    }
+    
+}
